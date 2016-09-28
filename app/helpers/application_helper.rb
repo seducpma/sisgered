42d9d7e154831d2f22418b477ae41d79c0b97d62
seducpma <naor@seducpma.com>
2016-09-28 09:04:35 -0300

    28-09-2016

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -84,9 +84,12 @@ module ApplicationHelper
    end
 
 def character_count(field_id, update_id, options = {})
-function = "$('#{update_id}').innerHTML = $F('#{field_id}').length;"
-out = javascript_tag(function) # set current length
-options = {:frequency => 0.1, :function => function}.merge(options) # default options
-out += observe_field(field_id, options) # and observe it
+  function = "$('#{update_id}').innerHTML = $F('#{field_id}').length;"
+  out = javascript_tag(function) # set current length
+  options = {:frequency => 0.1, :function => function}.merge(options) # default options
+  out += observe_field(field_id, options) # and observe it
 end
+
+
+
 end
