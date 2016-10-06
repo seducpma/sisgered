5dbed6913f254ccd9b4b8f03122e920f10e0a5f7
Seduc <seduc@pspma.(none)>
2016-10-06 14:51:42 -0300

    4-10-16

--- app/models/nota.rb
+++ app/models/nota.rb
@@ -6,5 +6,12 @@ class Nota < ActiveRecord::Base
   belongs_to :disciplina
   has_many :observacao_notas
   belongs_to :matricula
+
+
+ 
+ NOTASB =%w(10.0 9.0 8.0 7.0 6.0 5.0 4.0 3.0 2.0 1.0 0.0 TR RM F NF ABN)
+
+
+
  
 end
