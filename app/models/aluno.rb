431ae342874b89de2e5a5917cb2778ee366ac3b2
Seduc <seduc@pspma.(none)>
2016-10-20 15:28:39 -0200

    20-10-16

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -9,8 +9,8 @@ class Aluno < ActiveRecord::Base
   has_and_belongs_to_many :classes
   has_many :notas
   has_attached_file :photo, :styles => {:original=> "180x180>" },
-                    :url => "/photos/:class/:id_:basename.:extension",
-                    :path => ":rails_root/public/photos/:class/:id_:basename.:extension"
+                    :url => "/photos/:class/:id.:extension",
+                    :path => ":rails_root/public/photos/:class/:id.:extension"
 
 
   before_save  :maiusculo
