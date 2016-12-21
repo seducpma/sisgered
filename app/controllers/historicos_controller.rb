e9a6526a0242aa2eef4f37735e58b450e2fb25ed
Seduc <seduc@pspma.(none)>
2016-12-21 14:19:57 -0200

    21-12-16

--- app/controllers/historicos_controller.rb
+++ app/controllers/historicos_controller.rb
@@ -22,13 +22,15 @@ def final_resultado
      session[:classe_id]=params[:classe][:id]
      @classe = Classe.find(:all,:conditions =>['id = ?', params[:classe][:id]])
      @atribuicao_classe = Atribuicao.find(:all,:conditions =>['classe_id = ? AND ativo=?', params[:classe][:id],0])
+
      @matriculas = Matricula.find(:all,:conditions =>['classe_id = ?', params[:classe][:id]], :order => 'classe_num ASC')
      @notas = Nota.find(:all, :joins => "INNER JOIN atribuicaos ON atribuicaos.id = notas.atribuicao_id INNER JOIN disciplinas ON disciplinas.id = atribuicaos.disciplina_id", :conditions => ["atribuicaos.classe_id =? AND notas.ativo is null",  params[:classe][:id]],:order =>'disciplinas.ordem ASC')
      t=0
      @alunos = Aluno.find(:all, :joins => "inner join matriculas on alunos.id = matriculas.aluno_id", :conditions =>['matriculas.classe_id =?', params[:classe][:id]],:order =>' matriculas.classe_num')
      @matriculas_classe = Matricula.find(:all,:conditions =>['classe_id = ?',session[:classe_id]], :order => 'classe_num ASC')
+     @atribuicao_classe.length
 
-t=0
+
 
 
       render :update do |page|
