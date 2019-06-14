b9e274b54cd7499e4f831d28575e8457e9e430a9
Your Name <you@example.com>
2019-06-14 11:17:52 -0300

    14-06-2019

--- app/controllers/roles_users_controller.rb
+++ app/controllers/roles_users_controller.rb
@@ -45,9 +45,9 @@ class RolesUsersController < ApplicationController
   def index
     $new_id = 0
    if current_user.unidade_id == 53 or current_user.unidade_id == 52
-        @role_users = RolesUser.find(:all, :joins => :user, :order => 'login ASC')
+        @role_users = RolesUser.find(:all, :joins => :user, :conditions=> ['users.activated_at is not null'],:order => 'login ASC')
     else
-         @role_users = RolesUser.find(:all, :joins => :user,:conditions => ['users.unidade_id = ? ', current_user.unidade_id], :order => 'login ASC')
+         @role_users = RolesUser.find(:all, :joins => :user,:conditions => ['users.unidade_id = ? AND users.activated_at is not null', current_user.unidade_id], :order => 'login ASC')
     end
    respond_to do |format|
       format.html # index.html.erb
