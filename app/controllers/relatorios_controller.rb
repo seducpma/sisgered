1c3217ea9ca95fff8e814c6d1584b2907f718eef
Your Name <you@example.com>
2019-06-19 13:04:54 -0300

    20-06-20191200

--- app/controllers/relatorios_controller.rb
+++ app/controllers/relatorios_controller.rb
@@ -132,7 +132,7 @@ class RelatoriosController < ApplicationController
     @atribuicao = Atribuicao.find(:all, :conditions => ["professor_id =? and ano_letivo=?", session[:professor_id], Time.now.year ])
     cont_cl=0
     for atribuicao in @atribuicao
-        @aluno= Matricula.find(:all, :select => 'alunos.id, alunos.aluno_nome',:joins =>:aluno  ,:conditions=>['matriculas.classe_id=?', atribuicao.classe_id ], :order => 'alunos.aluno_nome ASC')
+        @aluno= Matricula.find(:all, :select => 'alunos.id, alunos.aluno_nome',:joins =>:aluno  ,:conditions=>['matriculas.classe_id=? AND (matriculas.status ="MATRICULADO" OR matriculas.status ="TRANSFERENCIA")', atribuicao.classe_id ], :order => 'alunos.aluno_nome ASC')
         if cont_cl==0
           @alunos2=@aluno
           cont_cl=cont_cl+1
