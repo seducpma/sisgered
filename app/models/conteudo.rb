69270d7bdbd243a737d08701e261887ad24a8f3c
Desenvolvimento <you@example.com>
2020-04-02 15:35:00 -0300

    2-04-2020-0927

--- app/models/conteudo.rb
+++ app/models/conteudo.rb
@@ -4,6 +4,13 @@ class Conteudo < ActiveRecord::Base
  belongs_to :professor
  belongs_to :unidade
  belongs_to :classe
+
+
+
+ validates_presence_of :classe_id
+ validates_presence_of :professor_id
+ validates_presence_of :unidade_id
+    validates_presence_of :atribuicao_id
  
 end
 
